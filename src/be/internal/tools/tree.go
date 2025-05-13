b8a879f2c908f82604b97967c862e5e5552e7c7a
BenedictPresley <19623252@std.stei.itb.ac.id>
2025-05-13 19:46:18 +0700

    fix: parser

--- src/be/internal/tools/tree.go
+++ src/be/internal/tools/tree.go
@@ -1,74 +1,106 @@
 package tools
 
 import (
-	"fmt"
 	"strings"
 )
 
-func ParseTree(s string) (*Node, error) {
-	s = strings.TrimSpace(s)
-
-	if len(s) == 0 {
-		return nil, fmt.Errorf("empty string")
+func ParseTree(expr string) (*Node, error) {
+	expr = strings.TrimSpace(expr)
+	if strings.HasPrefix(expr, "(") && strings.HasSuffix(expr, ")") {
+		expr = expr[1 : len(expr)-1]
 	}
 
-	i := 0
-	for i < len(s) && s[i] != '(' {
-		i++
+	openIdx := strings.Index(expr, "(")
+	if openIdx == -1 {
+		return &Node{Name: expr}, nil
 	}
 
-	if i == len(s) {
-		return &Node{Name: s}, nil
-	}
+	name := expr[:openIdx]
 
-	name := s[:i]
-
-	node := &Node{
+	root := &Node{
 		Name:     name,
 		Children: make([]*Node, 0, 2),
 	}
 
-	content := s[i:]
-	level := 0
-	start := 1
+	content := expr[openIdx:]
 
-	for j := 1; j < len(content); j++ {
-		if content[j] == '(' {
-			level++
-		} else if content[j] == ')' {
-			level--
-		}
+	var leftExpr, rightExpr string
 
-		if (level == 0 && j < len(content)-1 && content[j+1] == '(') || (level == -1) {
-			if j > start {
-				child, err := ParseTree(content[start : j+1])
-				if err != nil {
-					return nil, err
-				}
-				node.Children = append(node.Children, child)
+	leftStart := 0
+	parenCount := 0
+	leftEnd := 0
+
+	for i := 0; i < len(content); i++ {
+		if content[i] == '(' {
+			if parenCount == 0 {
+				leftStart = i
 			}
-
-			if level == -1 {
+			parenCount++
+		} else if content[i] == ')' {
+			parenCount--
+			if parenCount == 0 {
+				leftEnd = i
 				break
 			}
-
-			start = j + 1
 		}
 	}
 
-	return node, nil
+	if leftStart < leftEnd {
+		leftExpr = content[leftStart : leftEnd+1]
+	}
+
+	rightStart := leftEnd + 1
+	parenCount = 0
+	rightEnd := 0
+
+	for i := rightStart; i < len(content); i++ {
+		if content[i] == '(' {
+			if parenCount == 0 {
+				rightStart = i
+			}
+			parenCount++
+		} else if content[i] == ')' {
+			parenCount--
+			if parenCount == 0 {
+				rightEnd = i
+				break
+			}
+		}
+	}
+
+	if rightStart < rightEnd {
+		rightExpr = content[rightStart : rightEnd+1]
+	}
+
+	if leftExpr != "" {
+		leftNode, err := ParseTree(leftExpr)
+		if err != nil {
+			return nil, err
+		}
+		root.Children = append(root.Children, leftNode)
+	}
+
+	if rightExpr != "" {
+		rightNode, err := ParseTree(rightExpr)
+		if err != nil {
+			return nil, err
+		}
+		root.Children = append(root.Children, rightNode)
+	}
+
+	return root, nil
 }
 
 func CountNodes(node *Node) int {
 	if node == nil {
 		return 0
 	}
-	
+
 	count := 1
 	for _, child := range node.Children {
 		count += CountNodes(child)
 	}
-	
+
 	return count
 }
 
@@ -76,19 +108,19 @@ func SortTree(node *Node) *Node {
 	if node == nil || len(node.Children) < 2 {
 		return node
 	}
-	
+
 	for i := range node.Children {
 		node.Children[i] = SortTree(node.Children[i])
 	}
-	
+
 	if len(node.Children) == 2 {
 		leftCount := CountNodes(node.Children[0])
 		rightCount := CountNodes(node.Children[1])
-		
+
 		if rightCount > leftCount {
 			node.Children[0], node.Children[1] = node.Children[1], node.Children[0]
 		}
 	}
-	
+
 	return node
 }
